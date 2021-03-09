ea60b6c1f2aad648bb37108baa1b8232815c14fb
Sampada2999 <sampada_nandanwar@persistent.com>
2021-03-09 10:58:26 +0530

    modified add function

--- src/app/update.service.ts
+++ src/app/update.service.ts
@@ -1,26 +1,24 @@
 import { Injectable } from "@angular/core";
 import { Update } from "./update/update";
-import {Details} from "./Details";
+import { Details } from "./Details";
 @Injectable({
   providedIn: "root",
 })
 export class UpdateService {
   entry: Update[] = [];
-  json:Details[]=[]
- 
+  json: Details[] = [];
+
   constructor() {}
-  add(env, track, status) {
+
+  add(env, track, status, data, response, group) {
     this.entry.push(new Update(env, track, status));
-    this.json.push(new Details(track,status));
+    this.json.push(new Details(track, status, data, response, group));
   }
-  get():Update[]{
-   
+
+  get(): Update[] {
     return this.entry;
   }
   getjson(): {} {
     return JSON.stringify(this.json);
   }
-
-
-
 }
